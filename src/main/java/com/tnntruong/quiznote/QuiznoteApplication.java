d75d98369bdd11a7da8f4e6ea71c2950a7ce36f3
TuiTenTruong <truong0795432149@gmail.com>
2025-09-23 20:53:49 +0700

    first commit

---
+++ src/main/java/com/tnntruong/quiznote/QuiznoteApplication.java
@@ -0,0 +1,13 @@
+package com.tnntruong.quiznote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiznoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiznoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnntruong/quiznote/QuiznoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnntruong.quiznote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiznoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
